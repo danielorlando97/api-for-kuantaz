4315f8dd79d27f2195cac4893a20f444fa63ee5c
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-01-26 14:34:11 -0500

    add docs

--- src/__main__.py
+++ src/__main__.py
@@ -32,29 +32,3 @@ app.run(
     debug=environ.get("DEBUG_MODE", False),
     port=environ.get("PORT", 5000),
 )
-
-# https://stackabuse.com/using-sqlalchemy-with-flask-and-postgresql/
-# https://www.askpython.com/python-modules/flask/flask-postgresql
-# https://stackoverflow.com/questions/11774265/how-do-you-access-the-query-string-in-flask-routes
-
-# TODO: Validate inputs ✅✅
-#   https://stackoverflow.com/questions/61644396/flask-how-to-make-validation-on-request-json-and-json-schema
-#   https://stackoverflow.com/questions/57664997/how-to-return-400-bad-request-on-flask
-# TODO: Add Relations ✅✅ https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html
-# TODO: Add Dates ✅✅
-#   https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
-#   https://docs.sqlalchemy.org/en/14/core/defaults.html
-# TODO: Read Specifications ✅✅
-# TODO: Add Swagger  ✅✅
-#   https://flask-restplus.readthedocs.io/en/stable/quickstart.html
-#   https://github.com/flasgger/flasgger
-#   https://medium.com/analytics-vidhya/flasgger-an-api-playground-with-flask-and-swagger-ui-6b6806cf8884
-# TODO: Add UnitTest ✅✅
-#   https://testdriven.io/blog/flask-pytest/
-#   https://flask.palletsprojects.com/en/2.2.x/testing/
-# TODO: Docker and Env ✅✅
-#   https://testdriven.io/blog/dockerizing-flask-with-postgres-gunicorn-and-nginx/
-#   https://stackoverflow.com/questions/62807717/how-can-i-solve-postgresql-scram-authentication-problem
-# TODO: Create Readme
-#   docker-version
-#   python-version
